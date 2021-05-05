f15ede00c3e21e2767c314cda8885e4b8fe3adc9
1701闫昆鹏 <ykp@emaile.com>
2021-05-05 12:15:30 +0800

    增加收藏商品为空时的提示信息

--- pages/collect/index.js
+++ pages/collect/index.js
@@ -9,16 +9,22 @@ Page({
         weserv:app.globalData.weserv,
         //收藏数据
         collections: [],
-        islogin:false
+        islogin:false,
+        nocollection:false
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     async onShow(options) {
+        console.log("collection onShow")
         await app.getCollectionByOpenid()
-        const collections = wx.getStorageSync('collections');
-        this.setData({ collections })
+        var collections =await wx.getStorageSync('collections');
+        
+        if(collections.length==0)
+            this.setData({collections,nocollection:true})
+        else
+            this.setData({ collections ,nocollection:false})
         if(app.globalData.islogin)
             this.setData({islogin:true})
     },
